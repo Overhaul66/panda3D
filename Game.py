b48aa92194ced2991af3efb491bc824f6cdfa907
William <willadan667@gmail.com>
2023-07-11 14:32:17 +0000

    player class finished and moving

--- Game.py
+++ Game.py
@@ -5,13 +5,14 @@ from direct.task import Task
 from panda3d.core import WindowProperties
 from panda3d.core import AmbientLight
 from panda3d.core import DirectionalLight
-from panda3d.core import Vec4
-from panda3d.core import Vec3
+from panda3d.core import Vec4, Vec3
 from panda3d.core import CollisionTraverser
 from panda3d.core import CollisionHandlerPusher
 from panda3d.core import CollisionNode
 from panda3d.core import CollisionSphere
 from panda3d.core import CollisionTube
+
+from GameObject import Player
 
 class Game(ShowBase):
     
@@ -19,38 +20,35 @@ class Game(ShowBase):
         super().__init__()
 
         self.render.setShaderAuto()
-        
+
         properties = WindowProperties()
         properties.setSize(1000, 750)
-        properties.setTitle("Eli")
+        properties.setTitle("Game")
         self.win.requestProperties(properties)
 
         self.environment = self.loader.loadModel("Environment/environment")
         self.environment.reparentTo(self.render)
 
-        self.tempActor = Actor('models/act_p3d_chan', 
-            {
-                "walk":"models/a_p3d_chan_walk",
-            }
-        )
-        self.tempActor.reparentTo(self.render)
-        #self.tempActor.setPos(0,7,0)
-        #self.tempActor.getChild(0).setH(90)
-        self.tempActor.loop('walk')
+        self.pusher = CollisionHandlerPusher()
+        self.cTrav = CollisionTraverser()
 
 
         mainlight = DirectionalLight("main light")
-        #mainlight.setColor(Vec4(1,1,1,1))
         self.mainLightNodePath = self.render.attachNewNode(mainlight)
         self.mainLightNodePath.setHpr(45, -45, 0)
         self.render.setLight(self.mainLightNodePath)
+
+        ambientLight = AmbientLight("ambient light")
+        ambientLight.setColor(Vec4(0.2, 0.2, 0.2, 1))
+        self.ambientLightNodePath = render.attachNewNode(ambientLight)
+        render.setLight(self.ambientLightNodePath)
 
         self.accept('d', self.updateKeyMap, ["right", True] )
         self.accept('d-up', self.updateKeyMap, ["right", False] )
         self.accept('a', self.updateKeyMap, ["left", True] )
         self.accept('a-up', self.updateKeyMap, ["left", False] )
-        self.accept('w', self.updateKeyMap, ["top", True] )
-        self.accept('w-up', self.updateKeyMap, ["top", False] )
+        self.accept('w', self.updateKeyMap, ["up", True] )
+        self.accept('w-up', self.updateKeyMap, ["up", False] )
         self.accept('s', self.updateKeyMap, ["down", True] )
         self.accept('s-up', self.updateKeyMap, ["down", False] )
         
@@ -58,38 +56,12 @@ class Game(ShowBase):
         self.keyMap = {
             "right":False,
             "left":False,
-            'top':False,
+            'up':False,
             'down':False,
             'shoot':False,
         }
 
-        self.taskMgr.add(self.updatePlayer)
-
-        #collision
-        self.cTrav = CollisionTraverser()
-        self.pusher = CollisionHandlerPusher()
-
-        #create a collision node
-        colliderNode = CollisionNode("player")
-        #add a collision solid by creating a solid
-        colliderNode.addSolid(CollisionSphere(0,0,0,0.3))
-        #attach the collion node to the player
-        collider = self.tempActor.attachNewNode(colliderNode)
-        collider.show()
-
-        #tell the traverser and the pusher that the player is an object that should 
-        #collide with things
-
-        #the pusher wants collider and a nodePath of the player
-        self.pusher.addCollider(collider, self.tempActor)
-        #the transverser wants a collider and a handler
-        self.cTrav.addCollider(collider, self.pusher)
-
-        #prevents the pusher responses to be restricted to the horizontal 
-        #since we dealing with falt surfaces (2D)
-        self.pusher.setHorizontal(True)
-
-        wallSolid = CollisionTube(-8.0,0,0,8.0,0,0,0.2) #create a collison solid
+        wallSolid = CollisionTube(-8.0,0,0,8.0,0,0,0.2) #create a collision solid
         wallNode = CollisionNode("wall") # create a collision node (wall node)
         wallNode.addSolid(wallSolid) # add the collision solid to the wall node
         wall = self.render.attachNewNode(wallNode) # attach the wall node to the root node (render)
@@ -121,25 +93,22 @@ class Game(ShowBase):
         self.camera.setPos(0, 0, 32)
         self.camera.setP(-90)
 
+        self.taskMgr.add(self.updateTask, "update")
+
+        self.player = Player() 
+
         self.disableMouse()
 
     def updateKeyMap(self, controlName, controlState):
         self.keyMap[controlName] = controlState
 
-    def updatePlayer(self, task):
+    def updateTask(self, task):
         dt = globalClock.getDt()
-
-        if self.keyMap['right']:
-            self.tempActor.setPos(self.tempActor.getPos() + Vec3(10 * dt,0,0))
-        elif self.keyMap['left']:
-            self.tempActor.setPos(self.tempActor.getPos() + Vec3(-10 * dt,0,0))
-        elif self.keyMap['top']:
-            self.tempActor.setPos(self.tempActor.getPos() + Vec3(0,10 * dt,0))
-        elif self.keyMap['down']:
-            self.tempActor.setPos(self.tempActor.getPos() + Vec3(0,-10 * dt,0))
+        
+        self.player.update(self.keyMap, dt)
 
         return task.cont
-        
+
 
 game = Game()
 game.run()
--- GameObject.py
+++ GameObject.py
@@ -1,18 +1,14 @@
-from panda3d.core import Vec3, Vec4
-from panda3d.core import CollisionNode, CollisionSphere
+from panda3d.core import Vec3, Vec2
 from direct.actor.Actor import Actor
+from panda3d.core import CollisionSphere, CollisionNode
 
-FRICTION = 150
+FRICTION = 150.0
 
-class GameObject():
-    '''
-    Base class for game objects example player, enemy, 
 
-    '''
+class GameObject:
     def __init__(self, pos, modelName, modelAnims, maxHealth, maxSpeed, colliderName):
-        #set up the game objects model
         self.actor = Actor(modelName, modelAnims)
-        self.actor.reparentTo(self.render)
+        self.actor.reparentTo(render)
         self.actor.setPos(pos)
 
         self.maxHealth = maxHealth
@@ -25,74 +21,102 @@ class GameObject():
 
         self.walking = False
 
-        #collision node for the game object
         colliderNode = CollisionNode(colliderName)
         colliderNode.addSolid(CollisionSphere(0, 0, 0, 0.3))
         self.collider = self.actor.attachNewNode(colliderNode)
-
-        self.collider.setPythonTag("owner", self) # adds a reference of the GameObject to the collider
+        self.collider.setPythonTag("owner", self)
 
     def update(self, dt):
-        
-        speed = self.velocity.lenght()          #get speed of velocity
-
-        #if the speed of velocity is greater than maxSpeed 
+        speed = self.velocity.length()
+        # clamp speed 
         if speed > self.maxSpeed:
-            self.velocity.normalize()           # normalize the vector, direction is not affected
-            self.velocity *= self.maxSpeed      # set the velocity to maxSpeed
-            speed = self.maxSpeed               # speed will be altered, set it to maxSpeed
+            self.velocity.normalize()
+            self.velocity *= self.maxSpeed
+            speed = self.maxSpeed
 
-        if not self.walking:                    # if player is about stopping or has stopped 
+        # add friction to velocity
+        if not self.walking:
             frictionVal = FRICTION * dt
-            if frictionVal > speed:             
-                self.velocity.set(0,0,0)
+            if frictionVal > speed:
+                self.velocity.set(0, 0, 0)
             else:
                 frictionVec = -self.velocity
                 frictionVec.normalize()
                 frictionVec *= frictionVal
 
-                self.velocity += frictionVec    # add frictionVec to velocity to simulate friction
+                self.velocity += frictionVec
 
+        # update player position
         self.actor.setPos(self.actor.getPos() + self.velocity * dt)
 
-    
     def alterHealth(self, dHealth):
         self.health += dHealth
 
-        # make sure health does not exceed maxHealth
+        # clamp health
         if self.health > self.maxHealth:
             self.health = self.maxHealth
 
-    # removes and clears various component of objects associated with this class
+    # clean objec
     def cleanup(self):
-        
         if self.collider is not None and not self.collider.isEmpty():
-            self.collider.clearPythonTag("owner")    # removes the class associated with owner
-            base.cTrav.removeCollider(self.collider) # removes self.collider from the traverser 
-            base.pusher.removeCollider(self.collider)# removes self.collider from the pusher
+            self.collider.clearPythonTag("owner")
+            base.cTrav.removeCollider(self.collider)
+            base.pusher.removeCollider(self.collider)
 
         if self.actor is not None:
-            self.actor.cleanup() # clean ups the actor model
-            self.actor.removeNode() # removes the actor model form the node
-            self.actor = None 
+            self.actor.cleanup()
+            self.actor.removeNode()
+            self.actor = None
 
         self.collider = None
 
+
 class Player(GameObject):
-    '''
-    class for player , inherits GameObject
-    '''
-    pass
+    def __init__(self):
+        GameObject.__init__(
+            self,
+            Vec3(0, 0, 0),
+            "models/act_p3d_chan",
+            {"stand": "models/a_p3d_chan_idle", "walk": "models/a_p3d_chan_run"},
+            5,
+            10,
+            "player",
+        )
+        self.actor.getChild(0).setH(180)
 
+        base.pusher.addCollider(self.collider, self.actor)
+        base.cTrav.addCollider(self.collider, base.pusher)
 
-class Enemy(GameObject):
-    '''
-    Base class for all enemies, and subclass of GameObject
-    '''
-    pass
+        self.actor.loop("stand")
 
-class WalkingEnemy(Enemy):
-    '''
-    class for specific type of enemy
-    '''
-    pass
+    def update(self, keys, dt):
+        GameObject.update(self, dt)
+
+        self.walking = False
+
+        if keys["up"]:
+            self.walking = True
+            self.velocity.addY(self.acceleration * dt)
+        if keys["down"]:
+            self.walking = True
+            self.velocity.addY(-self.acceleration * dt)
+        if keys["left"]:
+            self.walking = True
+            self.velocity.addX(-self.acceleration * dt)
+        if keys["right"]:
+            self.walking = True
+            self.velocity.addX(self.acceleration * dt)
+
+        if self.walking:
+            standControl = self.actor.getAnimControl("stand")
+            if standControl.isPlaying():
+                standControl.stop()
+
+            walkControl = self.actor.getAnimControl("walk")
+            if not walkControl.isPlaying():
+                self.actor.loop("walk")
+        else:
+            standControl = self.actor.getAnimControl("stand")
+            if not standControl.isPlaying():
+                self.actor.stop("walk")
+                self.actor.loop("stand")
