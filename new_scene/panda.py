1b08bbf19574a55aa4c3d5658f7a427960234542
William <willadan667@gmail.com>
2023-08-01 09:53:42 +0000

    updated phoenix scene, player can use mouse to rotate the bird

--- new_scene/panda.py
+++ new_scene/panda.py
@@ -1,13 +1,29 @@
 from direct.showbase.ShowBase import ShowBase
 from direct.actor.Actor import Actor
-from panda3d.core import DirectionalLight, Vec4, Vec3
+from panda3d.core import DirectionalLight, Vec4, Vec3, WindowProperties
 from direct.task import Task
 from direct.gui.OnscreenText import OnscreenText
+
+import math
+
+def rotate(vector, angle):
+    x = vector.x * math.cos(angle) - vector.y * math.sin(angle)
+    y = vector.x * math.sin(angle) - vector.y * math.cos(angle)
+    z = vector.z
+    
+    return (x, y , z)
 
 class MyApp(ShowBase):
 
     def __init__(self):
         super().__init__()
+
+        self.props = WindowProperties()
+        self.props.setCursorHidden(True)
+        self.props.setMouseMode(WindowProperties.M_relative)
+        self.props.setFullscreen(True)
+
+        self.win.requestProperties(self.props)
 
         self.rot = 0
 
@@ -37,12 +53,43 @@ class MyApp(ShowBase):
         self.disable_mouse()
 
         taskMgr.add(self.move_phoenix, 'move phoenix')
+        taskMgr.add(self.mouse_movement, 'mouse_movemnt')
+
+        self.currentMouseX = 0
+        self.previousMouseX = 0
+        self.mouseMotion = False
        
 
     def move_phoenix(self, task):
         dt = globalClock.getDt()
+       
         self.phoenix_bird.setPos(self.phoenix_bird.getPos() + Vec3(1 * dt, 0, 0) )
+
         return task.cont 
+    
+    def mouse_movement(self, task):
+
+
+        if base.mouseWatcherNode.hasMouse():
+            self.currentMouseX = base.mouseWatcherNode.getMouseX()
+           
+
+        if self.currentMouseX != self.previousMouseX:
+            self.mouseMotion = True
+        else:
+            self.mouseMotion = False
+
+        self.previousMouseX = self.currentMouseX
+
+        mouse_sensitivity = 100 * globalClock.getDt()
+
+        if self.mouseMotion:
+            self.phoenix_bird.setH(self.phoenix_bird.getH() - self.currentMouseX * mouse_sensitivity)
+
+        
+       
+        return task.cont
+
 
 
 game = MyApp()
